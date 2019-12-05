905a5db9430b1c1da60f2a0e8ee31f755b93550a
Anastasia Schmitke <schmitke@mediamarktsaturn.com>
2019-12-05 10:27:13 +0100

    images insertion finished

--- player.js
+++ player.js
@@ -9,34 +9,30 @@ class MovingRectangle {
     this.speed = 0;
     this.positionX = x;
     this.positionY = y;
-    this.playerImg = new Image();
-    this.playerImg.src = "./images/Piedro.png";
-    this.carImg = new Image();
-    this.carImg.src = "./images.car2.png";
+    
+    this.img = new Image();
+    this.img.src = "";
     this.ctx = ctx;
     this.draw = this.draw.bind(this);
   }
 
  draw() {
-   this.ctx.fillStyle = this.color;
-  this.ctx.drawImage(this.playerImg, this.positionX, this.positionY, this.width, this.height);
+  //this.ctx.fillStyle = this.color;
+  debugger;
+  this.ctx.drawImage(this.img, this.positionX, this.positionY, this.width, this.height);
   //this.ctx.fillRect(this.positionX, this.positionY, this.width, this.height);
   }
 }
 
 class Player extends MovingRectangle {
-  constructor(x, y, ctx) {
+  constructor(x, y, imgSrc, ctx) {
     super(x, y, "red", 50, 50, ctx);
-
+    this.img.src = imgSrc; //"./images/Piedro.png";
     this.points = 9400;
     this.lifes = 3;
     this.carrots = 0;
   }
-  //draw() {
-    //this.ctx.fillStyle = this.color;
-    //this.ctx.drawImage(this.playerImg, this.positionX, this.positionY, this.width, this.height);
-  //}
-  
+
   looseLife() {
     this.lifes -= 1;
     return this.lifes;
